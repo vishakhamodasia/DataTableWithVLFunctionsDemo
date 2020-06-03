a00351e3dc8150e3865a92dba9ee02d4e352d4a5
Vishakha Yogendra Modasia <vmodasia@altimetrik.com>
2020-06-03 17:11:49 +0530

    first commit

---
+++ src/main/java/com/example/controller/BatteryController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BatteryController {
+	
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String goHome(){
+		return "index";
+	}
+	
+	@RequestMapping(path="/one", method=RequestMethod.GET)
+	public String goToOne(){
+		return "one";
+	}
+	
+	@RequestMapping(path="/two", method=RequestMethod.GET)
+	public String goToTwo(){
+		return "two";
+	}
+
+}
